7c02b41ad50e59177baf6620ac0093a1ce328039
vikingjs <vikingjs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-28 22:48:05 +0000

    Version 1!
    git-svn-id: https://plugins.svn.wordpress.org/suicide-squirrel-alert-broadcasting-system/trunk@158758 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,66 @@
+=== Suicide Squirrel Threat Advisory System ===
+Contributors: vikingjs
+Donate link: http://muddledramblings.com/suicide-squirrel-widget/
+Tags: silly, useless, squirrel
+Requires at least: 2.7 and PHP 5
+Tested up to: 2.8.4
+Stable tag: trunk
+
+Adds a sidebar widget to alert the masses to the current Suicide Squirrel Threat Level.
+
+== Description ==
+
+This widget will check in with the central command center and display the appropriate graphic reflecting the current Suicide Squirrel Threat Level. The threat level is based on rigorous scientific research, monitoring squirrel 'chatter' in channels generally associated with squirrel activity.
+
+The Suicide Squirrel Advisory System is designed with Science to guide our protective measures when specific information to a particular sector or geographic region is received. ÊIt combines threat information with vulnerability assessments and provides communications to public safety officials and the public. Ê
+
+The Color-coded Threat Level System is used to communicate with public safety officials and the public at-large through a threat-based, color-coded system so that protective measures can be implemented to reduce the likelihood or impact of squirrel suicide.
+
+Note: I copied a lot of the above from The Department of Homeland Security. You have to admit it's a masterpiece of bureaucratic quack-talk.
+
+== Installation ==
+
+1. Upload `squirrel-threat.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place the widget in your sidebar.
+1. Configure your own snide commentary.
+1. Decide if you want the widget to link back to the Advisory Center page.
+1. Sit back and take pride in your role making the world a safer place.
+
+== Frequently Asked Questions ==
+
+= Why in the name of all that is holy would I want a garish, badly-drawn, inconsequential widget taking up precious space in my sidebar? =
+
+Because if you don't, the squirrels have won.
+
+= It doesn't work! =
+
+That's not a question.
+
+= Why doesn't it work? =
+
+That's better, but not very specific. This is a pretty simple Widgit, but it requires that your server has simpleXML. simpleXML is turned on by default for (almost) any server running PHP5. Do you get a message about simpleXML? Why didn't you say so!
+
+= Is that all it does? Put up an ugly picture? =
+
+NO! Not only does it put up an ugly picture, it sets the alt and title attribures of the picture as well! Optioinally, it links back to the Command Cennter Advisory page so your viewers will know what to do.
+
+== Screenshots ==
+
+1. Condition yellow! Protect your nuts!
+
+== Changelog ==
+
+= 1.0 =
+* First release.
+
+== Future ==
+
+We here at Squirrel Alert Command Center never rest in our quest to make the world a safer place. It's possible that in the future this widget will become even more sophisticated. Maybe not likely, but possible.
+
+Future enhancements might include:
+
+* customizable messages for every alert level
+* option to use smaller versions of the images
+* anything any clever nutjob out there suggests. (That means you.)
+
---
+++ squirrel-threat.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Plugin Name: Suicide Squirrel Threat Meter
+Description: Adds a sidebar widget that connects to the Suicide Squirrel Advisory Broadcasting System, and loads the correct image to indicate the current threat of suicidal squirral activity, as established by SCIENCE! Do your part to counter the Global Squirrel Insurgency!
+Author: Jerry Seeger
+License: GPL vs3.0 or higher.
+Version: 1.0
+Author URI: http://muddledramblings.com
+*/
+
+// Put functions into one big function we'll call at the plugins_loaded
+// action. This ensures that all required plugin functions are defined.
+function widget_squirrel_threat_init() {
+
+	if ( !function_exists('register_sidebar_widget') )
+		return;
+
+	function jsh_get_squirrel_threat_data() {
+		$url='http://muddledramblings.com/current-squirrel-threat';
+		$data = NULL;
+		
+		// Check for 404? Doesn't seem necessary since page is static. If server's down woun't get 404 anyway.
+		
+		$raw_data = wp_remote_fopen($url); // more flexible than using simplexml_load_file (I think)
+		
+		//print_r($raw_data);
+
+		if ($raw_data) {
+			if (!function_exists('simplexml_load_string') ) {
+				echo 'Dude, you need PHP 5 on your server for this. If you have PHP 5 but turned off simpleXML, go and turn it back on. The Squirrels compel you.';
+			}
+			else {
+				$data = simplexml_load_string($raw_data);
+			}
+			
+		}
+		else {
+			echo "<br>Error connecting to Suicide Squirrel Alert Broadcasting Syetem. Protect your nuts and pray to whatever's handy that this isn't the start of something big.";
+		}
+
+		return $data;
+	}
+	
+	function widget_squirrel_threat($args) {
+		
+		// $args is an array of strings that help widgets to conform to
+		// the active theme: before_widget, before_title, after_widget,
+		// and after_title are the array keys. Default tags: li and h2.
+		extract($args);
+
+		$data = jsh_get_squirrel_threat_data();
+		if ($data) {
+			$imageURI = $data->imageURI;
+			$linkURI = $data->linkURI;
+			$chatter = $data->chatter;
+			$color = $data->color;
+	
+			$options = get_option('widget_squirrel_threat');
+			$title = $options['title'];
+			$remark = $options['remark'];
+			$do_link = $options['do_link'];
+			
+			echo $before_widget . $before_title . $title . $after_title;
+			
+	
+			echo '<div class="sstl_display" style="text-align:center;">';
+			if ($do_link == 'Yes') {
+				echo '<a href="'.$linkURI.'">';
+			}
+			echo '<img src="'.$imageURI.'" alt="Suicide Squirrel Alert Level" title="Suicide Squirrel Alert Level: '.$color.'; chatter: '.$chatter.'">';
+			if ($do_link == 'Yes') {
+				echo '</a>';
+			}
+			echo '</div>';
+			
+			echo '<p class="sstl_remark">'.$remark.'</p>';
+			echo $after_widget;
+		} else {
+			echo "<br /> Level 1 Squirrel Emergency! PANIC!"
+		}
+	}
+
+	function widget_squirrel_threat_control() {
+
+		// Get options and see if we're handling a form submission.
+		$options = get_option('widget_squirrel_threat');
+		if ( !is_array($options) )
+			$options = array('title'=>'', 'remark'=>'', 'do_link'=>'Yes');
+		if ( $_POST['squirrel_threat-submit'] ) {
+			// Remember to sanitize and format use input appropriately.
+			$options['title'] = strip_tags(stripslashes($_POST['squirrel_threat-title']));
+			$options['remark'] = strip_tags(stripslashes($_POST['squirrel_threat-remark']));
+			$options['do_link'] = $_POST['do_link'];
+			update_option('widget_squirrel_threat', $options);
+		}
+
+		// Be sure you format your options to be valid HTML attributes.
+		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		$remark = htmlspecialchars($options['remark'], ENT_QUOTES);
+		$do_link = $options['do_link'];
+		
+		echo '<p style="text-align:right;"><label for="squirrel_threat-title">' . __('Title:') . ' <input style="width: 200px;" id="squirrel_threat-title" name="squirrel_threat-title" type="text" value="'.$title.'" /></label></p>';
+		echo '<p style="text-align:right;"><label for="squirrel_threat-remark">' . __('Remark:', 'widgets') . ' <input style="width: 200px;" id="squirrel_threat-remark" name="squirrel_threat-remark" type="text" value="'.$remark.'" /></label></p>';
+		
+		$checked = $do_link == 'Yes' ? ' CHECKED' : '';
+		echo '<p style="text-align:right;"><label for="squirrel_threat-remark">' . __('Link to explanation:', 'widgets') . ' <input id="squirrel_threat-link" name="do_link" type="checkbox" value="Yes"'.$checked.' /> </label></p>';
+		
+		echo '<input type="hidden" id="squirrel_threat-submit" name="squirrel_threat-submit" value="1" />';
+	}
+	
+	register_sidebar_widget(array('Suicide Squirrel Threat', 'widgets'), 'widget_squirrel_threat');
+	register_widget_control(array('Suicide Squirrel Threat', 'widgets'), 'widget_squirrel_threat_control', 300, 130);
+}
+
+// Run our code later in case this loads prior to any required plugins.
+add_action('widgets_init', 'widget_squirrel_threat_init');
+
+?>
